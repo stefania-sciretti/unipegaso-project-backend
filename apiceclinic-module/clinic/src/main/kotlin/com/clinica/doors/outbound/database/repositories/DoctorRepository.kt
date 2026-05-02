6ed33d4f4fd981bfa3deca0d650db0184ce130b3
stefania-sciretti <stefania.sciretti@gmail.com>
2026-05-02 17:14:11 +0200

    removed classes related to doctor; use specialist instead

--- apiceclinic-module/clinic/src/main/kotlin/com/clinica/doors/outbound/database/repositories/DoctorRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.clinica.doors.outbound.database.repositories
-
-import com.clinica.doors.outbound.database.entities.DoctorEntity
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface DoctorRepository : JpaRepository<DoctorEntity, Long>
